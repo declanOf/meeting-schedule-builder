81180fb72e0aa8038e06acc3559e63122d13ab21
Declan Ó F <declanofiaich@gmail.com>
2024-03-14 19:52:35 -0600

    Lint fix

--- js/lib/array-equals.js
+++ js/lib/array-equals.js
@@ -15,8 +15,8 @@ Array.prototype.equals = function (array) {
     if (array === this) {
         return true;
     }
-    
-    // compare lengths - can save a lot of time 
+
+    // compare lengths - can save a lot of time
     if (this.length != array.length) {
         return false;
     }
@@ -26,19 +26,18 @@ Array.prototype.equals = function (array) {
         if (this[i] instanceof Array && array[i] instanceof Array) {
             // recurse into the nested arrays
             if (!this[i].equals(array[i]))
-                return false;       
-        }           
-        else if (this[i] != array[i]) { 
+                return false;
+        }
+        else if (this[i] != array[i]) {
             // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;   
-        }           
+            return false;
+        }
     }
     return true;
 }
 
 // Hide method from for-in loops
 Object.defineProperty(Array.prototype, "equals", {enumerable: false});
-
 
 Object.defineProperty(Array.prototype, "pluck", {
     value: function(key) {
